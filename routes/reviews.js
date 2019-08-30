e517a5f0e7f9111b26202e506c97ef2e25eebb6b
EmildaZhang <emildazhg07@gmail.com>
2019-08-30 10:29:28 +0700

    adding update and delete feature, and Auth for CRUD Review

--- routes/reviews.js
+++ routes/reviews.js
@@ -10,9 +10,28 @@ const isLoggedIn = (req, res, next) => {
   res.redirect("/login");
 };
 
+const checkReveiwOwnership = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    Review.findById(req.params.review_id, (err, review) => {
+      if (err) {
+        res.redirect("back");
+      } else {
+        if (review.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          res.redirect("back");
+        }
+      }
+    });
+  } else {
+    res.redirect("back")
+  }
+}
+
+
 router.get("/new", isLoggedIn, (req, res) => {
   Book.findById(req.params.id, (err, data) => {
-    err ? console.log(err) : res.render("review/addReview", { data: data });
+    err ? console.log(err) : res.render("review/add", { data: data });
   });
 });
 
@@ -20,7 +39,7 @@ router.post("/", isLoggedIn, (req, res) => {
   Book.findById(req.params.id, (err, book) => {
     if (err) {
       console.log(err);
-      redirect("/books");
+      res.redirect("back");
     } else {
       Review.create(req.body.review, (err, review) => {
         if (err) {
@@ -38,5 +57,38 @@ router.post("/", isLoggedIn, (req, res) => {
     }
   });
 });
+
+
+router.get("/:review_id/edit", checkReveiwOwnership, (req, res) => {
+  Review.findById(req.params.review_id, (err, review) => {
+    if (err) {
+      console.log(err);
+      res.redirect("back")
+    } else {
+      res.render("review/edit", { data_id: req.params.id, review: review });
+    }
+  })
+})
+
+router.put("/:review_id", checkReveiwOwnership, (req, res) => {
+  Review.findByIdAndUpdate(req.params.review_id, req.body.review, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.redirect("back")
+    } else {
+      res.redirect(`/books/${req.params.id}`);
+    }
+  })
+})
+
+router.delete("/:review_id", checkReveiwOwnership, (req, res) => {
+  Review.findByIdAndRemove(req.params.review_id, (err, review) => {
+    if (err) {
+      res.redirect(`/books/${req.params.id}`)
+    } else {
+      res.redirect(`/books/${req.params.id}`)
+    }
+  })
+})
 
 module.exports = router;
